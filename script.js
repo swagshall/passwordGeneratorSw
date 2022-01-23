a745aa1e4a6c33094ee025b75dae6dab8b23c64b
Shifra Wagshall <shifrawagshall@gmail.com>
2022-01-22 21:43:25 -0500

    Added charecters to the passChar var if user wants to include that type of char.

--- script.js
+++ script.js
@@ -19,11 +19,20 @@ var incLow;
 var incUp;
 var incNum;
 var incSpec;
-var password= document.getElementById("password");
+var passChar='';
+
+const charSet ={
+ lower : "abcdefghijklmnopqrstuvwxyz", 
+ upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+ numbers: "0123456789",
+ spec: "!@#$%^&*()?/|"
+
+}
 // var lower= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];;
 // var upper= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 // var numeric= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // var specChar=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "|"];
+
 
 
 
@@ -35,23 +44,23 @@ function generatePassword(){
           passLen();
        
 
-            for (var i = 0; i <= numChar; i++) {
-                //do if inc =true or...=true next comment
-                if ((incLow==true) && (incUp==true)&&
-                (incNum==true)&&(incSpec==true)){
+        //     for (var i = 0; i <= numChar; i++) {
+        //         //do if inc =true or...=true next comment
+        //         if ((incLow==true) && (incUp==true)&&
+        //         (incNum==true)&&(incSpec==true)){
 
-                    var fullCharSet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "|" ] ;
+        //             var fullCharSet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "|" ] ;
 
-                var randomNumber = Math.floor(Math.random() * fullCharSet.length);
-                password += fullCharSet[i].substring(randomNumber, randomNumber +1);
-            }
-            // console.log(document.getElementById("password").value = password);
-            console.log(password);
+        //         var randomNumber = Math.floor(Math.random() * fullCharSet.length);
+        //         password += fullCharSet[i].substring(randomNumber, randomNumber +1);
+        //     }
+        //     // console.log(document.getElementById("password").value = password);
+        //     console.log(password);
 
-        }
+        // }
                     
 
-            return "password goes here";
+        //     return "password goes here";
         }
         else if (!proceed) {
         //don't proceed
@@ -84,13 +93,25 @@ function incSpecial(){
     
     if(include){
         incLow=confirm("Would you like to include lowercase letters?  ");
-        console.log("Include lower: " +incLow);
+        console.log("Include lower: " +incLow); 
+            if (incLow== true){
+                [passChar+=charSet.lower];
+            }
         incUp=confirm("Would you like to include uppercase letters?  ");
         console.log("Include upper: " +incUp);
+             if (incUp== true){
+                [passChar+=charSet.upper];
+             }
         incNum=confirm("Would you like to include numeric letters?  ");
         console.log("Include numbers: " +incNum);
+                if (incNum== true){
+                    [passChar+=charSet.numbers];
+                }
         incSpec=confirm("Would you like to include special charcters?  ");
         console.log("Include spec: " +incSpec);
+                if (incSpec== true){
+                    [passChar+=charSet.spec];
+                }
 
             if ((incLow==false) && (incUp==false)&&
             (incNum==false)&&(incSpec==false)){
@@ -101,6 +122,7 @@ function incSpecial(){
             }
             else{
                 console.log("call to generare function" );
+                console.log("password chars include: "+passChar );
             }    
        
          //call to function that generates random password
@@ -112,6 +134,6 @@ function incSpecial(){
     }    
     
 }
-function genPass(numChar, incLow, incUp, incNum, incSpec){
+// function genPass(numChar, incLow, incUp, incNum, incSpec){
 
-}
+// }
