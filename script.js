6421bb432f57626b70495c982040c9199b81a388
Shifra Wagshall <shifrawagshall@gmail.com>
2022-01-22 21:35:11 -0500

    Added obj to hold all the charecters.

--- script.js
+++ script.js
@@ -19,10 +19,11 @@ var incLow;
 var incUp;
 var incNum;
 var incSpec;
-var lower= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];;
-var upper= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numeric= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specChar=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "|"];
+var password= document.getElementById("password");
+// var lower= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];;
+// var upper= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// var numeric= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var specChar=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "|"];
 
 
 
@@ -32,12 +33,25 @@ function generatePassword(){
         if (proceed) {
             numChar= prompt("Please enter the length you want your password to be: ");
           passLen();
+       
 
-            // for (var i = 0; i <= numChar; i++) {
-            //     var randomNumber = Math.floor(Math.random() * chars.length);
-            //     password += chars.substring(randomNumber, randomNumber +1);
-            // }
-                    document.getElementById("password").value = password;
+            for (var i = 0; i <= numChar; i++) {
+                //do if inc =true or...=true next comment
+                if ((incLow==true) && (incUp==true)&&
+                (incNum==true)&&(incSpec==true)){
+
+                    var fullCharSet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "|" ] ;
+
+                var randomNumber = Math.floor(Math.random() * fullCharSet.length);
+                password += fullCharSet[i].substring(randomNumber, randomNumber +1);
+            }
+            // console.log(document.getElementById("password").value = password);
+            console.log(password);
+
+        }
+                    
+
+            return "password goes here";
         }
         else if (!proceed) {
         //don't proceed
@@ -97,4 +111,7 @@ function incSpecial(){
         incSpecial();
     }    
     
+}
+function genPass(numChar, incLow, incUp, incNum, incSpec){
+
 }
